c41f4e132a7443e52e563f6aad99a96561264ef4
Yue Zhang <architectyuezhang@gmail.com>
2022-11-02 17:13:33 -0400

    made dummy data

---
+++ utils/seeds.tsx
@@ -0,0 +1,71 @@
+import { AiOutlineShopping, AiOutlineFire } from "react-icons/ai";
+import { BsMusicNoteBeamed, BsTrophy } from "react-icons/bs";
+import { RiMovieLine } from "react-icons/ri";
+import { IoLogoGameControllerA } from "react-icons/io";
+import { BiNews } from "react-icons/bi";
+import { GrCoatCheck } from "react-icons/gr";
+import { TbDogBowl } from "react-icons/tb";
+
+export const topics = [
+  {
+    name: "music",
+    icon: <BsMusicNoteBeamed />,
+  },
+  {
+    name: "shopping",
+    icon: <AiOutlineShopping />,
+  },
+  {
+    name: "movie",
+    icon: <RiMovieLine />,
+  },
+  {
+    name: "sport",
+    icon: <BsTrophy />,
+  },
+  {
+    name: "gaming",
+    icon: <IoLogoGameControllerA />,
+  },
+  {
+    name: "news",
+    icon: <BiNews />,
+  },
+  {
+    name: "fashion",
+    icon: <GrCoatCheck />,
+  },
+  {
+    name: "animal",
+    icon: <TbDogBowl />,
+  },
+  {
+    name: "trending",
+    icon: <AiOutlineFire />,
+  },
+];
+
+export const footerList1 = [
+  "About",
+  "Newsroom",
+  "Store",
+  "Contact",
+  "Carrers",
+  "ByteDance",
+  "Creator Directory",
+];
+export const footerList2 = [
+  "TikTik for Good",
+  "Advertise",
+  "Developers",
+  "Transparency",
+  "TikTik Rewards",
+];
+export const footerList3 = [
+  "Help",
+  "Safety",
+  "Terms",
+  "Privacy",
+  "Creator Portal",
+  "Community Guidelines",
+];
